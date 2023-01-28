589cb5775c085c5d012ba45f302f65f7d979c955
M Picheta <74721362+tama3ti@users.noreply.github.com>
2023-01-28 15:48:59 +0100

    update okhttpclient version

--- src/com/cr5315/jSmite/HttpClient.java
+++ src/com/cr5315/jSmite/HttpClient.java
@@ -1,8 +1,8 @@
 package com.cr5315.jSmite;
 
-import com.squareup.okhttp.OkHttpClient;
-import com.squareup.okhttp.Request;
-import com.squareup.okhttp.Response;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 import java.io.IOException;
 
